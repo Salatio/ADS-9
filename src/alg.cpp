280943553ccce57ea2913b2a014f5258a3ff7dd3
Salatio <99189453+Salatio@users.noreply.github.com>
2022-05-29 16:23:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,25 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> Ttree;
+    std::ifstream file(filename);
+    
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+    char ch;
+    std::string word = "";
+    while(!file.eof()) {
+        ch = file.get();
+        if ((symb >= 65 && symb <= 90) || (symb >= 97 && symb <= 122))
+            word += tolower(ch);
+        else {
+            Ttree.add(word);
+            word = "";
+        }
+    }
+    
+    file.close();
+    return Ttree;
 }
